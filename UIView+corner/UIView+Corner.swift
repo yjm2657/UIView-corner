4c126c87c912b33ca018a15224b462b1ee57bf44
YJM <490964242@qq.com>
2017-07-27 15:40:42 +0800

    uiview圆角
    uiview圆角_swift

---
+++ UIView+corner/UIView+Corner.swift
@@ -0,0 +1,32 @@
+//
+//  UIView+Corner.swift
+//  UIView+corner
+//
+//  Created by YJM on 2017/7/27.
+//  Copyright © 2017年 YJM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIView {
+    /// 部分圆角
+    /// - Parameters:
+    ///   - corners: 需要实现为圆角的角，可传入多个
+    ///   - radii: 圆角半径
+    func corner(byRoundingCorners corners:UIRectCorner, cornerRadii: CGFloat) {
+        let maskPath = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: cornerRadii, height: cornerRadii))
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = self.bounds
+        maskLayer.path = maskPath.cgPath
+        self.layer.mask = maskLayer
+    }
+    /// 全部圆角
+    /// - Parameters:
+    ///   - radii: 圆角半径
+    func cornerAllRound(cornerRadii: CGFloat) {
+        self.corner(byRoundingCorners: UIRectCorner.allCorners, cornerRadii: cornerRadii)
+    }
+    
+}
--- UIView+corner/ViewController.swift
+++ UIView+corner/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let aView:UIView = UIView(frame: CGRect(x: 20, y: 50, width: 100, height: 100))
+        aView.backgroundColor = UIColor.blue
+        aView.corner(byRoundingCorners: [UIRectCorner.topLeft, UIRectCorner.bottomRight], cornerRadii: 10)
+        self.view.addSubview(aView)
+        
+        
+        let bView:UIView = UIView(frame: CGRect(x: 170, y: 50, width: 100, height: 100))
+        bView.backgroundColor = UIColor.red
+        bView.cornerAllRound(cornerRadii: 10)
+        self.view.addSubview(bView)
+        
     }
 
     override func didReceiveMemoryWarning() {
